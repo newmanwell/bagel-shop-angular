d095aa6b9e3c392cd32325c07fd2c04ca8b595dc
Shaun Newman <shaun.newman.p@gmail.com>
2026-01-08 10:53:19 -0500

    chore: initial commit with base angular config

---
+++ src/app/app.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  imports: [],
+  template: ` <h1>BAngular Shop</h1> `,
+  styleUrls: ['./app.css'],
+})
+export class App {
+  title = 'Bagel Shop pt 2';
+}
