644822fe04fc24a340e13659ef0546e9f0ff4e03
Shaun Newman <shaun.newman.p@gmail.com>
2026-01-13 15:24:12 -0500

    added constructor to customize routing by id

--- src/app/details/details.ts
+++ src/app/details/details.ts
@@ -1,15 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-details',
   imports: [],
   template: `
     <p>
-      details works!
+      details works! {{ bagelLocationId }}
     </p>
   `,
   styles: ``,
 })
 export class Details {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  bagelLocationId = 0;
 
+  constructor() {
+    this.bagelLocationId = Number(this.route.snapshot.params["id"]);
+  }
 }
