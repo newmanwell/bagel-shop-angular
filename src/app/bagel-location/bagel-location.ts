a4f9119aade4745afeab99ab903bb9e0ebde99cc
Shaun Newman <shaun.newman.p@gmail.com>
2026-01-13 15:15:48 -0500

    feat: added routing to details component vis id

--- src/app/bagel-location/bagel-location.ts
+++ src/app/bagel-location/bagel-location.ts
@@ -1,16 +1,18 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BagelLocationInterface } from '../bagel-location';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-bagel-location',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   template: `
     <section class="bagel">
       <img class="bagel-photo" [src]="bagelLocation.photo" alt="photo of {{ bagelLocation.bagelName }}" />
       <h2 class="bagel-heading">{{ bagelLocation.bagelName }}</h2>
       <p class="bagel-location">{{ bagelLocation.bagelDescription }}</p>
+      <a [routerLink]="['/details', bagelLocation.id ]">More Bagel Info</a>
     </section>
   `,
   styleUrls: [`./bagel-location.css`],
--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1,11 +1,17 @@
 import { Routes } from '@angular/router';
 import { Home } from './home/home';
+import { Details } from './details/details';
 
 const routeConfig: Routes = [
   {
     path: '',
     component: Home,
     title: 'Home Page'
+  },
+  {
+    path: 'details/:id',
+    component: Details,
+    title: 'Details Page'
   }
 ];
 
