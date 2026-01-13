85c72132d8455aa26588c11c2dea499a1ba199cd
Shaun Newman <shaun.newman.p@gmail.com>
2026-01-13 14:57:47 -0500

    feat: imported provideRouter in main.ts, added routes.ts and created route for the home page

---
+++ src/app/routes.ts
@@ -0,0 +1,12 @@
+import { Routes } from '@angular/router';
+import { Home } from './home/home';
+
+const routeConfig: Routes = [
+  {
+    path: '',
+    component: Home,
+    title: 'Home Page'
+  }
+];
+
+export default routeConfig;
