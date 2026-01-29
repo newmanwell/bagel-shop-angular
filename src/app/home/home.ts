c7df4bcab6768ea7a70ed0f6344f4eae30f918c7
Shaun Newman <shaun.newman.p@gmail.com>
2026-01-29 10:00:58 -0500

    feat: added all bagels component and moved all the bagel data to the new component

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -1,42 +1,19 @@
-import { Component, inject } from '@angular/core';
+import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { BagelLocation } from '../bagel-location/bagel-location';
-import { BagelLocationInterface } from '../bagel-location';
-import { Bagel } from '../bagel';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule, BagelLocation],
+  imports: [CommonModule, RouterModule],
   template: `
     <section>
-      <form>
-        <input type="text" placeholder="Filter by bagel" #filter>
-        <button class="primary" type="button" (click)="filterResults(filter.value)">Search</button>
-      </form>
-    </section>
-    <section class="results">
-      <app-bagel-location *ngFor="let bagelLocation of filteredBagelList" [bagelLocation]="bagelLocation"></app-bagel-location>
+      <a [routerLink]="['/allbagels']">Our Bagels</a>
     </section>
   `,
   styleUrls: ['./home.css'],
 })
 
 export class Home {
-  bagelLocationList: BagelLocationInterface[] = [];
-  bagel: Bagel = inject(Bagel);
-  filteredBagelList: BagelLocationInterface[] = [];
-
-  constructor() {
-    this.bagelLocationList = this.bagel.getAllBagels();
-    this.filteredBagelList = this.bagelLocationList;
-  }
-
-  filterResults(text: string) {
-    if (!text) this.filteredBagelList = this.bagelLocationList;
-
-    this.filteredBagelList = this.bagelLocationList.filter(
-      bagelLocationList => bagelLocationList?.bagelName.toLowerCase().includes(text.toLowerCase())
-    );
-  }
+  
 }
