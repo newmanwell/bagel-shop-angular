0fdc1d1777ee33bb4b3d629322679e866868e403
Shaun Newman <shaun.newman.p@gmail.com>
2026-02-17 14:42:35 -0500

    chore: added price to the interface and the bagelLocationList injectable

--- src/app/bagel.ts
+++ src/app/bagel.ts
@@ -15,6 +15,7 @@ export class Bagel {
       bagelName: 'Plain',
       photo: '/assets/bagel_pictures/plain.webp',
       bagelDescription: 'A plain bagel, our most special',
+      price: 200,
       availableBagels: 22,
       creamCheese: true,
       butter: true
@@ -24,6 +25,7 @@ export class Bagel {
       bagelName: 'Everything',
       photo: '/assets/bagel_pictures/everything.jpg',
       bagelDescription: 'Everything on the kitchen counter for this one',
+      price: 250,
       availableBagels: 14,
       creamCheese: true,
       butter: true
@@ -33,6 +35,7 @@ export class Bagel {
       bagelName: 'Rainbow',
       photo: '/assets/bagel_pictures/rainbow.jpg',
       bagelDescription: 'Taste the rainbow! Its just a plain bagel with color',
+      price: 300,
       availableBagels: 6,
       creamCheese: true,
       butter: true
@@ -42,6 +45,7 @@ export class Bagel {
       bagelName: 'Salt',
       photo: '/assets/bagel_pictures/salt.jpg',
       bagelDescription: 'Salty like the beach',
+      price: 300,
       availableBagels: 9,
       creamCheese: true,
       butter: false
@@ -51,6 +55,7 @@ export class Bagel {
       bagelName: 'Egg',
       photo: '/assets/bagel_pictures/egg.webp',
       bagelDescription: 'Are eggs cheap again?',
+      price: 400,
       availableBagels: 5,
       creamCheese: false,
       butter: true
@@ -60,6 +65,7 @@ export class Bagel {
       bagelName: 'Pumpernickel',
       photo: '/assets/bagel_pictures/pumpernickel.webp',
       bagelDescription: 'Not a chocolate bagel',
+      price: 300,
       availableBagels: 2,
       creamCheese: false,
       butter: true
@@ -69,6 +75,7 @@ export class Bagel {
       bagelName: 'Hot Cheetos',
       photo: '/assets/bagel_pictures/flaming-hot-bagel.jpg',
       bagelDescription: 'Oh she spicy',
+      price: 500,
       availableBagels: 4,
       creamCheese: true,
       butter: false
